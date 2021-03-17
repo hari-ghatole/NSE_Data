fa0b524aaaf0f81d35f4f0b9ed79d611cbfb16c7
hari-ghatole <hari.ghatole@gmail.com>
2021-03-17 13:43:20 +0530

    Get Data

--- GetData_workinprogress.py
+++ GetData_workinprogress.py
@@ -1,17 +1,17 @@
 import numpy as np
 import talib 
-#from nsepy import get_history as gh
+from nsepy import get_history as gh
 import datetime as dt 
 
 
 #import values from nsepy and print technical indicator values
-#symbol = 'BANKNIFTY'
-#start = dt.date(2018,1,1)
-#end = dt.date(2020,12,31)
+symbol = 'BANKNIFTY'
+start = dt.date(2020,1,1)
+end = dt.date(2021,1,1)
 
 
 #get data
-#data = gh(symbol, start, end, index=True)
+data = gh(symbol, start, end, index=True)
 
 bnf_open = list((data["Open"]))
 bnf_high = list((data["High"]))
@@ -19,18 +19,19 @@ bnf_low = list((data["Low"]))
 bnf_close = list((data["Close"]))
 bnf_vol = list((data["Volume"]))
 
-'''
 for i in range(len(bnf_close)):
     ohlclist = list((bnf_open[i],bnf_high[i],bnf_low[i],bnf_close[i]))
     print((ohlclist))
-'''
+
 
 open_arr = np.array(bnf_open)
 high_arr = np.array(bnf_high)
 low_arr = np.array(bnf_low)
 close_arr = np.array(bnf_close)
 
-############### CALCULATION ############### 
+
+'''
+### RSI CALCULATION ###
 
 relative_calc = np.ndarray.tolist(talib.RSI(close_arr, timeperiod=11))
 
@@ -43,7 +44,8 @@ for i in range(len(bnf_close)):
     elif relative_calc[i] > 20 and relative_calc[i]<80:
         print(relative_calc[i],"===\n")
     i = i +1
-print("Value of i is:",i)
-print("LENGHT:",len(close_arr))
 
-#atr_calc = np.ndarray.tolist(ta.ATR(high_arr,low_arr,close_arr,timeperiod=14))
+'''
+
+print("Value of i is:",i)
+print("LENGHT:",len(bnf_close))
